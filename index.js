b0d3c9d430689516e88b8f9ce18481ba2bf663fa
Froren <r@sager>
2016-10-04 20:56:31 -0700

    Initial commit for leaderboard

---
+++ index.js
@@ -0,0 +1,23 @@
+var
+	express = require('express'),
+	app = express(),
+  cfenv = require('cfenv'),
+  appEnv = cfenv.getAppEnv(),
+	io = require('socket.io').listen(app.listen(appEnv.port)),
+	hbs = require('hbs');
+
+
+app.engine('html', hbs.__express);
+app.set('view engine', 'hbs');
+app.set('views', './');
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.send('Hello ' + appEnv.url);
+});
+
+// io.on('connection', function(socket) {
+//
+//   socket.once('action', function() {
+//   });
+// });
