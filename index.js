3febb2383929d6f7dcdbc37086bc82e3a093f905
Randall Ho <randallh@ca.ibm.com>
2016-10-13 19:06:10 -0400

    Added ability to render multiple games and saved states

--- index.js
+++ index.js
@@ -25,24 +25,28 @@ io.on('connection', function(socket) {
     });
 });
 
-var amqpUrl = process.env.amqpUrl;
+if (process.env.amqpUrl) {
 
-if (amqpUrl) {
+    //Connect to the rabbitmq instance
     amqp.connect(amqpUrl, function(err, conn) {
         conn.createChannel(function(err, channel) {
+
+            //Assert the queues and bind the exchange to it
             channel.assertQueue('', {exclusive: true}, function(err, ok){
 
                 channel.bindQueue(ok.queue, 'msg', '#');
+
+                //Listen on new events
                 channel.consume(ok.queue, function(msg) {
 
                     var gameObject = JSON.parse(msg.content);
                     io.sockets.emit('boardUpdate', gameObject);
 
                     //Update array of past games
-                    pastGames.unshift(gameObject);
+                    pastGames.push(gameObject);
 
                     if (pastGames.length > 5) {
-                        pastGames.splice(5);
+                        pastGames.splice(0, 1);
                     }
 
                 }, {noAck: true});
